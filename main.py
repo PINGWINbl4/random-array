36fe52d90fba543d2b23a8651c229959498cfa68
Ping2win <40270221+PINGWINbl4@users.noreply.github.com>
2021-12-08 23:46:21 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 from loguru import logger
+# Создаем лог файл
 logger.add("log.log", format="{time} {level} {message}", level="DEBUG")
 print('Введите целое число - количество эллементов')
 try:
@@ -10,13 +11,15 @@ except(Exception):
     logger.info(n)
     logger.error('')
     print('Вы ввели некорректоное число')
+# Формируем строку из аллементов
 a = []
 for i in range(n):
     a.append(i+1)
-
+# Пишем случайный элемент(если быть точнее, эллемент со случайным индексом)
 while a !=[]:
     x = random.randint(0, n-1)
     print(a[x], end=' ')
+# Удаляем выведенный элемент
     n -= 1
     a.pop(x)
 print()
