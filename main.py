a18170a9c45ad36a563b0af41e1ec2b3c31fee58
Ping2win <40270221+PINGWINbl4@users.noreply.github.com>
2021-12-09 10:51:00 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ except(Exception):
     print('Вы ввели некорректоное число')
 # Формируем строку из аллементов
 a = []
+new_a = []
 for i in range(n):
     a.append(i+1)
 # Пишем случайный элемент(если быть точнее, эллемент со случайным индексом)
 while a !=[]:
     x = random.randint(0, n-1)
     print(a[x], end=' ')
+    new_a.append(a[x])
 # Удаляем выведенный элемент
     n -= 1
     a.pop(x)
+logger.info(new_a, n)
 print()
-logger.info(n)
